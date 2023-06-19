c4e71f998ffcffdc7f305f0047d0606e9f94a0a1
Felix Lighto <137007836+felixlighto@users.noreply.github.com>
2023-06-19 16:58:21 +0200

    explain is changed

--- url_parcalama/app2.js
+++ url_parcalama/app2.js
@@ -3,7 +3,7 @@ var url = require("url");
 
 http.createServer(function (req, res) {
 
-    var q = url.parse(req.url, true).query;     //http://localhost:8080/?name=mustafa&lastname=senyuz&country=germany
+    var q = url.parse(req.url, true).query;     //http://localhost:8080/?name=mustafa&lastname=senyuz&country=germany    this url to browse
     var txt = "firstname :" + q.name + ", lastname :" + q.lastname + ", country : " + q.country;
     console.log(req.url);
     res.end(txt);
